f19044864a7b19db15a3d47efc6c6a9f762a8d96
capenuelas-afk <capenuelas@unincca.edu.co>
2026-03-13 18:02:15 -0500

    Create ProductoRepository.cs

---
+++ CleanArchitecture.Infrastructure/Data/Repositories/CleanArchitecture.Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,49 @@
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Producto>> GetAllAsync()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        public async Task<Producto?> GetByIdAsync(int id)
+        {
+            return await _context.Productos.FindAsync(id);
+        }
+
+        public async Task AddAsync(Producto producto)
+        {
+            await _context.Productos.AddAsync(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Producto producto)
+        {
+            _context.Productos.Update(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
